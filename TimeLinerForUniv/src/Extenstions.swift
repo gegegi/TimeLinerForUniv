d90c016f6aed8494326400e9018cdd6612657326
gegegi <developerhui@gmail.com>
2019-04-08 23:42:59 +0900

    190408 - init

---
+++ TimeLinerForUniv/src/Extenstions.swift
@@ -0,0 +1,9 @@
+//
+//  Extenstions.swift
+//  TimeLinerForUniv
+//
+//  Created by GEGEGi on 08/04/2019.
+//  Copyright © 2019 GEGEGi. All rights reserved.
+//
+
+import Foundation
---
+++ TimeLinerForUniv/src/View/TimeStackView.swift
@@ -0,0 +1,21 @@
+//
+//  TimeStackView.swift
+//  TimeLinerForUniv
+//
+//  Created by GEGEGi on 08/04/2019.
+//  Copyright © 2019 GEGEGi. All rights reserved.
+//
+
+import UIKit
+
+class TimeStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
